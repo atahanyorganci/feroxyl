1e4a096f2100054b45fb56512ef4c1f1a79ad6c4
Atahan Yorgancı <atahanyorganci@hotmail.com>
2026-02-18 20:26:10 +0300

    feat: configure `taplo` with `treefmt-nix`

--- treefmt.nix
+++ treefmt.nix
@@ -6,7 +6,12 @@
         deadnix.enable = true;
         mdsh.enable = true;
         alejandra.enable = true;
+        taplo.enable = true;
       };
+      settings.formatter.taplo.options = [
+        "--config"
+        (builtins.toString ./taplo.toml)
+      ];
     };
   };
 }
