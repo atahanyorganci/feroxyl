2b3f25b9731e906ce19022b72e0fc3707c58294c
Atahan Yorgancı <atahanyorganci@hotmail.com>
2026-02-19 10:56:51 +0300

    feat: add `rustfmt` to `treefmt`

--- treefmt.nix
+++ treefmt.nix
@@ -3,9 +3,10 @@
     treefmt = {
       projectRootFile = "flake.nix";
       programs = {
+        alejandra.enable = true;
         deadnix.enable = true;
         mdsh.enable = true;
-        alejandra.enable = true;
+        rustfmt.enable = true;
         taplo.enable = true;
       };
       settings.formatter.taplo.options = [
