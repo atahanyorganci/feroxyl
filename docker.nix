9532af7e000e28f40905ef0c9e63a32b1f719b83
Atahan Yorgancı <atahanyorganci@hotmail.com>
2026-02-19 22:53:28 +0300

    feat: add missing `architecture` argument to `dockerTools.buildLayeredImage`

--- docker.nix
+++ docker.nix
@@ -7,10 +7,15 @@
     cargoTomlContents = builtins.readFile ./Cargo.toml;
     cargoToml = builtins.fromTOML cargoTomlContents;
     name = cargoToml.package.name;
+    systemToArchitecture = {
+      "x86_64-linux" = "amd64";
+      "aarch64-linux" = "arm64";
+    };
+    architecture = systemToArchitecture.${pkgs.stdenv.system};
   in {
     packages = pkgs.lib.mkIf pkgs.stdenv.isLinux {
       feroxyl-image = pkgs.dockerTools.buildLayeredImage {
-        inherit name;
+        inherit name architecture;
         config = {
           Cmd = [
             "${self'.packages.feroxyl}/bin/feroxyl"
